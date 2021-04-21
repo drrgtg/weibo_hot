46d8c3da1dafa853cbaa97b639470ce6d462f503
Json_var <412032320@qq.com>
2021-04-21 09:28:41 +0800

    Update mod.ts
    delete zhihu video

--- mod.ts
+++ mod.ts
@@ -1,11 +1,9 @@
-import { zhihuVideo } from "./zhihu-video.ts";
 import { zhihuQuestions } from "./zhihu-questions.ts";
 import { zhihuSearch } from "./zhihu-search.ts";
 import { weiboSearch } from "./weibo-search.ts";
 import { toutiaoSearch } from "./toutiao-search.ts";
 
 export async function init() {
-  await zhihuVideo();
   await zhihuQuestions();
   await zhihuSearch();
   await weiboSearch();

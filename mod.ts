a1fbb6b465639d4b1b2a2dd79d6d7dc6e86eaeba
Json_var <412032320@qq.com>
2021-04-24 13:58:23 +0800

    Update mod.ts
    add zhihu video task

--- mod.ts
+++ mod.ts
@@ -2,12 +2,14 @@ import { zhihuQuestions } from "./zhihu-questions.ts";
 import { zhihuSearch } from "./zhihu-search.ts";
 import { weiboSearch } from "./weibo-search.ts";
 import { toutiaoSearch } from "./toutiao-search.ts";
+import { zhihuVideo } from "./zhihu-video.ts";
 
 export async function init() {
   await zhihuQuestions();
   await zhihuSearch();
   await weiboSearch();
   await toutiaoSearch();
+  await zhihuVideo();
 }
 
 init();
